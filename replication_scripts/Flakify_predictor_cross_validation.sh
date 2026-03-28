5352a524f5139ad0c5ab16f58c06d25560ddd07d
NAMU GO <ng2349@nyu.edu>
2026-03-28 17:43:53 +0400

    Hugging Face resolved by downloading the model locally

---
+++ replication_scripts/Flakify_predictor_cross_validation.sh
@@ -0,0 +1,9 @@
+#Flakify's flaky test prediction using cross-validation
+dataset=$1
+data_path="../dataset"
+replication_results_path="../replication_results"
+dataset_file="${data_path}/${dataset}/Flakify_${dataset}_dataset.csv" 
+model_weights="${replication_results_path}/REPLICATED_model_weights_${dataset}_dataset.pt"
+results_file="${replication_results_path}/REPLICATED_results_${dataset}_dataset.csv"
+
+python3 Flakify_predictor_cross_validation.py $dataset_file $model_weights $results_file
